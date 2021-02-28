63dc17afd69c67ed59e12bc5fb8ae1b32c5faa95
lalith <lalithkmr94@gmail.com>
2021-02-28 19:31:42 +0530

    stock table patch

--- patch_2021_feb/stock_table.sql
+++ patch_2021_feb/stock_table.sql
@@ -25,3 +25,18 @@ CHANGE COLUMN `avl_qty` `avl_qty` INT NULL DEFAULT NULL AFTER `personName`;
 -- CHANGE COLUMN `avl_g_wt` `avl_g_wt` FLOAT NULL DEFAULT 0 ,
 -- CHANGE COLUMN `avl_n_wt` `avl_n_wt` FLOAT NULL DEFAULT 0 ,
 -- CHANGE COLUMN `avl_p_wt` `avl_p_wt` FLOAT NULL DEFAULT 0 ;
+
+ALTER TABLE `gsprod`.`stock_1` 
+CHANGE COLUMN `gross_wt` `gross_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `net_wt` `net_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `pure_wt` `pure_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `sold_g_wt` `sold_g_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `sold_n_wt` `sold_n_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `sold_p_wt` `sold_p_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `avl_g_wt` `avl_g_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `avl_n_wt` `avl_n_wt` DECIMAL(6,3) NULL DEFAULT NULL ,
+CHANGE COLUMN `avl_p_wt` `avl_p_wt` DECIMAL(6,3) NULL DEFAULT NULL ;
+
+
+ALTER TABLE `gsprod`.`stock_1` 
+ADD COLUMN `uid` VARCHAR(45) NOT NULL DEFAULT NULL AFTER `id`;
