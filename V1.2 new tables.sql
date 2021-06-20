b9594a491ecc5a7e5af1b0d7932cd045f8c9581b
lalithkumart <lalithkmr94@gmail.com>
2021-06-20 19:42:05 +0530

    Updated the fund related tables

--- V1.2 new tables.sql
+++ V1.2 new tables.sql
@@ -1,29 +1,29 @@
-DROP TABLE IF EXISTS `fund_houses`;
-CREATE TABLE `fund_houses` (
+DROP TABLE IF EXISTS `fund_accounts`;
+CREATE TABLE `fund_accounts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-);
+)
 
-INSERT INTO `fund_houses` (`name`) VALUES ('shop');
-INSERT INTO `fund_houses` (`name`) VALUES ('bank');
+INSERT INTO `fund_accounts` (`name`) VALUES ('shop');
+INSERT INTO `fund_accounts` (`name`) VALUES ('bank');
 
 
 DROP TABLE IF EXISTS `fund_transactions`;
 CREATE TABLE `fund_transactions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `fund_house_id` varchar(45) DEFAULT NULL,
+  `account_id` varchar(45) DEFAULT NULL,
   `transaction_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `amount` int NOT NULL DEFAULT '0',
+  `gs_uid` varchar(45) DEFAULT NULL,
+  `cash_in` int NOT NULL DEFAULT '0',
+  `cash_out` int DEFAULT '0',
   `category` varchar(45) DEFAULT NULL,
   `remarks` text,
+  `deleted` tinyint DEFAULT '0',
   `created_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `modified_date` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gs_uid_UNIQUE` (`gs_uid`)
 );
-
-ALTER TABLE `gsprod`.`fund_transactions` 
-ADD COLUMN `cash_out` INT NULL DEFAULT 0 AFTER `cash_in`,
-CHANGE COLUMN `amount` `cash_in` INT NOT NULL DEFAULT 0 ;
