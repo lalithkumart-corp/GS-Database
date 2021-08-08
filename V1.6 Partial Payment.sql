2617898fc7e58e1acfbdec15cb272141d0431498
lalithkumart <lalithkmr94@gmail.com>
2021-08-08 20:59:18 +0530

    Updates for fund_transaction table

--- V1.6 Partial Payment.sql
+++ V1.6 Partial Payment.sql
@@ -1,3 +1,9 @@
 ALTER TABLE `gsprod`.`fund_transactions` 
 DROP INDEX `gs_uid_UNIQUE` ,
 ADD UNIQUE INDEX `gs_uid_UNIQUE` (`gs_uid` ASC, `user_id` ASC, `category` ASC) VISIBLE;
+
+ALTER TABLE `gsprod`.`fund_transactions` 
+DROP INDEX `gs_uid_UNIQUE` ;
+
+ALTER TABLE `gsprod`.`fund_transactions` 
+ADD COLUMN `customer_id` VARCHAR(45) NULL AFTER `user_id`;
