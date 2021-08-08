b5213e3b3952f0f410e6818f74a4a262f7c01299
lalithkumart <lalithkmr94@gmail.com>
2021-08-08 09:38:09 +0530

    minor query update

--- V1.6 Partial Payment.sql
+++ V1.6 Partial Payment.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `gsprod-dev`.`fund_transactions` 
+ALTER TABLE `gsprod`.`fund_transactions` 
 DROP INDEX `gs_uid_UNIQUE` ,
-ADD UNIQUE INDEX `gs_uid_UNIQUE` (`gs_uid` ASC, `category` ASC) VISIBLE;
+ADD UNIQUE INDEX `gs_uid_UNIQUE` (`gs_uid` ASC, `user_id` ASC, `category` ASC) VISIBLE;
