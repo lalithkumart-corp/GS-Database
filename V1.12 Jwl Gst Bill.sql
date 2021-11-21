99cca2a5bc1db828ba4e56c235e0feaee975e936
lalithkumart <lalithkmr94@gmail.com>
2021-11-21 19:09:14 +0530

    Update reg the Jewellery Selling Panel module

--- V1.12 Jwl Gst Bill.sql
+++ V1.12 Jwl Gst Bill.sql
@@ -2,11 +2,16 @@ ALTER TABLE `stores`
 ADD COLUMN `gst_no` VARCHAR(155) NULL DEFAULT NULL AFTER `email`;
 
 
-CREATE TABLE `gsprod`.`jewellery_bill_avl_template_list` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `template_id` INT NULL,
-  `screenshot_url` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `jewellery_bill_avl_template_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(45) NOT NULL,
+  `template_id` int DEFAULT NULL,
+  `screenshot_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `gsprod`.`jewellery_bill_avl_template_list` (`category`, `template_id`, `screenshot_url`) VALUES ('gst', '1', '/uploads/system/jewellery_gst_bill_template1.jpeg');
+
 
 CREATE TABLE `jewellery_bill_settings` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -19,4 +24,33 @@ CREATE TABLE `jewellery_bill_settings` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `gsprod`.`jewellery_bill_template_settings` (`user_id`, `category`, `selected_template`) VALUES ('1', 'gst', '1');
+INSERT INTO `gsprod`.`jewellery_bill_settings` (`user_id`, `category`, `bill_series`, `bill_no`, `selected_template`) VALUES ('1', 'gst', 'A', '1', '1');
+
+-- Adding new col "wastage_val" in stock_sold_1
+ALTER TABLE `gsprod`.`stock_sold_1` 
+ADD COLUMN `wastage_val` FLOAT NULL AFTER `wastage`;
+
+-- Alter in old_items_stock_1 table
+ALTER TABLE `gsprod`.`old_items_stock_1` 
+DROP COLUMN `user_id`,
+DROP COLUMN `qty`,
+DROP COLUMN `prod_id`,
+DROP COLUMN `ornament`,
+DROP COLUMN `retail_rate`,
+DROP COLUMN `wastage`,
+DROP COLUMN `touch_id`,
+ADD COLUMN `item_type` VARCHAR(45) NULL AFTER `user_id`,
+ADD COLUMN `wastage_val` FLOAT NULL AFTER `wastage`,
+CHANGE COLUMN `invoice_ref` `invoice_ref` VARCHAR(45) NULL DEFAULT NULL ;
+
+ALTER TABLE `gsprod`.`old_items_stock_1` 
+CHANGE COLUMN `id` `id` INT NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `gsprod`.`invoice_details_1` 
+ADD COLUMN `invoice_no` VARCHAR(45) NOT NULL AFTER `ukey`;
+
+ALTER TABLE `gsprod`.`invoice_details_1` 
+ADD COLUMN `raw_data` TEXT NULL AFTER `raw_payment_data`;
+
+ALTER TABLE `gsprod`.`invoice_details_1` 
+RENAME TO  `gsprod`.`jewellery_invoice_details_1` ;
